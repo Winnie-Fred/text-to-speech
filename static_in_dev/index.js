204786240eab9fec5d2ac1136d399c77ff86861e
Winnie <winiigboama@gmail.com>
2023-01-28 19:52:19 +0100

    print debugging

--- static_in_dev/index.js
+++ static_in_dev/index.js
@@ -121,11 +121,6 @@ async function uploadToServer(elementName, objectToUpload, csrftoken, url) {
     .catch((response) => {
         console.log("response status: ", response.status);
         console.log("response status text: ", response.statusText)
-        
-        // get error messages, if any
-        response.then((res) => {
-        console.log("Error message: ", res);
-        })
 
         const error_msg = ["An error occured while trying to communicate with the server.",
                     "Sorry about that. Please try again."
