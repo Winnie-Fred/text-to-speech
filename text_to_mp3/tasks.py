b6be4de68158e90b586a2943e9d4e35672da4722
Winnie <winiigboama@gmail.com>
2023-07-24 23:51:32 +0100

    upgrade: use direct download link for mp3 download
    clicking on the download button now directly downloads the mp3 file.
    It previously used the resource link which opened the mp3 in the
    browser with a download option.
    
    diff --git a/templates/conversion_successful.html b/templates/conversion_successful.html
    index 0f608e4..f9fab16 100644
    --- a/templates/conversion_successful.html
    +++ b/templates/conversion_successful.html
    @@ -30,7 +30,7 @@
                             <div class="mediaplayerwrapper" id="mediaPlayerWrapper">
                                 <div class="mediaplayer" id="mediaPlayer">
    
    -                                <audio id="audioPlayer" src="{{ speech_mp3 }}"></audio>
    +                                <audio id="audioPlayer" src="{{ speech_src_mp3 }}"></audio>
    
                                     <div class="discarea">
                                         <div class="disc">
    @@ -61,7 +61,7 @@
                                 </div>
    
                             </div>
    -                        <a href="{{ speech_mp3 }}" title="Download" class="download" download>Download</a>
    +                        <a href="{{ speech_download_mp3 }}" title="Download" class="download" download>Download</a>
                         </div>
                     </div>
                     {% comment %} Modal {% endcomment %}
    diff --git a/text_to_mp3/tasks.py b/text_to_mp3/tasks.py
    index b7c6232..faa9de9 100644
    --- a/text_to_mp3/tasks.py
    +++ b/text_to_mp3/tasks.py
    @@ -32,9 +32,12 @@ def convert_text_to_speech(self, text, lang, tld, file_name, context):
                 resource_type='video'
             )
             mp3_info = cloudinary.api.resource(file_name, resource_type='video')
    -        src_url = mp3_info['secure_url']
    -        context["speech_mp3"] = src_url
    +        src_url = mp3_info['secure_url']
    +        # Retrieve the URL for the downloadable MP3
    +        downloadable_url = cloudinary.utils.cloudinary_url(file_name, resource_type='video', transformation={'flags': f'attachment:{file_name}'})[0]
    
    +        context["speech_src_mp3"] = src_url
    +        context["speech_download_mp3"] = downloadable_url
             context["file_name"] = file_name[:15] + '...' + '.mp3' if len(file_name) > 15 else file_name + '.mp3'
             html = render_block_to_string('conversion_successful.html', 'content', context)
             return {"html": html, "context": context}

--- text_to_mp3/tasks.py
+++ text_to_mp3/tasks.py
@@ -32,9 +32,12 @@ def convert_text_to_speech(self, text, lang, tld, file_name, context):
             resource_type='video'
         )
         mp3_info = cloudinary.api.resource(file_name, resource_type='video')
-        src_url = mp3_info['secure_url']
-        context["speech_mp3"] = src_url
+        src_url = mp3_info['secure_url'] 
+        # Retrieve the URL for the downloadable MP3
+        downloadable_url = cloudinary.utils.cloudinary_url(file_name, resource_type='video', transformation={'flags': f'attachment:{file_name}'})[0]
 
+        context["speech_src_mp3"] = src_url
+        context["speech_download_mp3"] = downloadable_url
         context["file_name"] = file_name[:15] + '...' + '.mp3' if len(file_name) > 15 else file_name + '.mp3'
         html = render_block_to_string('conversion_successful.html', 'content', context)
         return {"html": html, "context": context}
