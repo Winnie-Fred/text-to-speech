019c3579c806466fbe645a3a872b213ed145d42a
Winnie <winiigboama@gmail.com>
2024-01-06 01:46:48 +0100

    build: add build steps specific for production
    collect static files, create superuser and perform migrations on db.

--- start.sh
+++ start.sh
@@ -9,6 +9,12 @@ celery -A text_to_speech worker -l info &
 
 # Start Django server
 if [ -n "$LIVE" ]; then
+    python manage.py collectstatic --noinput
+    if [[ $CREATE_SUPERUSER ]];
+    then
+    python manage.py createsuperuser --no-input
+    fi
+    python manage.py migrate
     gunicorn text_to_speech.wsgi:application -c gunicorn_config.py
 else
     python manage.py runserver 0.0.0.0:8000
