ce88b7170dbc1bbb3137d1b9b38ff27dbe279ff9
Winnie <winiigboama@gmail.com>
2024-02-01 22:51:15 +0100

    build: use docker in production again
    use docker in production again, otherwise, celery does not receive tasks
    while running in the background on free tier.

--- start.sh
+++ start.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-pip install -r requirements.txt
+# Start supervisord in the background
+supervisord -n &
 
 # Start Celery worker
 celery -A text_to_speech worker -l info &
 
-
 python manage.py collectstatic --noinput
+
 python manage.py migrate
+
 
 if [ "$CREATE_SUPERUSER" = "True" ];
 then
   echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
   echo "Created superuser"
 fi
+
+gunicorn text_to_speech.wsgi:application
