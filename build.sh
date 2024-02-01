3aa84e73505c058c102488e5b9132ff0b5a7a28c
Winnie <winiigboama@gmail.com>
2024-02-01 22:24:46 +0100

    build: provide credentials to create superuser with
    remove check for production or development since build file will only
    be used in deployment. Provide credentials to create superuser with
    through environment vars.

--- build.sh
+++ build.sh
@@ -6,14 +6,11 @@ pip install -r requirements.txt
 celery -A text_to_speech worker -l info &
 
 
-# Start Django server
-if [ -n "$RENDER" ]; then
-    python manage.py collectstatic --noinput
-    if [[ $CREATE_SUPERUSER ]];
-    then
-    python manage.py createsuperuser --no-input
-    fi
-    python manage.py migrate
-else
-    python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+if [ "$CREATE_SUPERUSER" = "True" ];
+then
+  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+  echo "Created superuser"
 fi
